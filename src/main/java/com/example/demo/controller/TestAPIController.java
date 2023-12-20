c6057834211ef7ddae7ffb13c811e8a51c5feab5
techcongty <techcongty75@gmail.com>
2023-12-21 01:03:05 +0700

    as

---
+++ src/main/java/com/example/demo/controller/TestAPIController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/gate-way/")
+public class TestAPIController {
+
+	@GetMapping("test")
+	public ResponseEntity<?> getMethodName() {
+		return new ResponseEntity("Call API Successfully",HttpStatus.OK);
+	}
+	
+}
